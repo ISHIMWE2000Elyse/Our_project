1836cbd71b9fed87a32f5bfc70b39292003a29ac
ISHIMWE2000Elyse <elyseishimwe11@gmail.com>
2025-12-29 23:07:02 +0200

    Elimy

--- Store/urls.py
+++ Store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import product_list
 from . import views
 
 urlpatterns = [
-    path('products/', product_list, name='product_list'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/add/', views.product_create, name='product_create'),  # ✅ ADD PRODUCT
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
     path('products/<int:pk>/edit/', views.product_edit, name='product_edit'),
     path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
--- Store/views.py
+++ Store/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 @login_required
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
     return render(request, 'store/product_list.html', {'products': products})
 
 
@@ -37,3 +37,20 @@ def product_delete(request, pk):
     product = get_object_or_404(Product, pk=pk)
     product.delete()
     return redirect('product_list')  # ✅ FIXED
+
+@login_required
+def product_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        price = request.POST.get('price')
+        inventory = request.POST.get('inventory')
+
+        Product.objects.create(
+            title=title,
+            price=price,
+            inventory=inventory
+        )
+
+        return redirect('product_list')
+
+    return render(request, 'store/product_create.html')
